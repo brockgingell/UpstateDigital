449c6db3bb1714881674cc20d5143007ac96c18b
brockgingell <brockg68@icloud.com>
2023-10-20 09:15:32 -0400

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import WhyUs from '@/components/landing-page/why-us'
 
 export default function Home() {
   return (
-    <main className='container absolute flex flex-col isolate w-screen gap-8 p-0'>
+    <main className='flex flex-col items-center isolate w-screen gap-8 p-0'>
       <Hero />
       <WhyUs />
     </main>
--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -5,20 +5,20 @@ import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { buttonVariants } from '@/components/ui/button'   
 import { motion } from 'framer-motion'
-import Devices from 'public/herofinal.svg'
+import Heropic from 'public/herofinal.svg'
 
 export default function Hero() {
   return (
-      <section className='container mt-8 flex w-screen flex-col items-center self-center justify-center'>
+      <section className='container mt-12 flex w-screen flex-col items-center self-center justify-center'>
         <div className='flex flex-col max-w-[1200px] items-center justify-center lg:flex-row'>
-          <div className='flex flex-col w-[300px] sm:w-[600px] items-center gap-3 justify-evenly'>
-            <h1 className='max-w-[400px] text-3xl text-center font-bold'>
+          <div className='flex flex-col w-[300px] mobilemd:w-[360px] tablet:w-[540px] sm:w-[600px] items-center gap-2 justify-evenly'>
+            <h1 className='max-w-[400px] text-3xl mobilemd:text-4xl text-center font-bold'>
               Elevate Your Business with a Modern Website
             </h1>
             <h2 className='max-w-[400px] text-xl text-center text-muted-foreground font-medium'>
               Don't let your outdated website hold your business back any longer. Stay ahead.
             </h2>
-            <div className='mt-2 flex flex-col w-full items-center gap-3'>
+            <div className='mt-4  flex flex-col w-full items-center gap-3'>
               <motion.div whileHover={{ scale: 1.1, transition: { duration: 1 }}} whileTap={{ scale: 0.8, transition: { duration: 1 } }} >
                 <Link href={siteConfig.mainNav[2].href} className={`w-full px-8 max-w-[400px] ${buttonVariants({variant: 'secondary'})}`}>
                   Book a Free Consultation →
@@ -31,7 +31,7 @@ export default function Hero() {
               </motion.div>
             </div>
           </div>
-          <Devices className='flex h-auto w-[300px] sm:w-[600px]'/>
+          <Heropic className='flex h-auto w-[300px] sm:w-[600px]'/>
         </div>
         <svg className='flex animate-bounce' xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
           <circle cx='12' cy='12' r='10'/>
