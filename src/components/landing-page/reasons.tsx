ad7969f1cf0b24236ba50e38b3d26ca7e0a0a67e
brockgingell <brockg68@icloud.com>
2023-10-20 15:19:06 -0400

    new

--- src/components/landing-page/reasons.tsx
+++ src/components/landing-page/reasons.tsx
@@ -1,9 +1,13 @@
-import Heropic from 'public/seopic.svg'
+import Designpic from 'public/designpic.svg'
+import Seo from 'public/seopic.svg'
+import Custom from 'public/custompic.svg'
 
 export default function Reasons() {
   return (
     <div>
-      <Heropic className='flex h-auto w-[280px] xs:w-[360px] md:w-[420px] xl:w-[560px]'/>
+      <Designpic className='flex h-auto w-[280px] xs:w-[360px] md:w-[420px] xl:w-[560px]'/>
+      <Seo className='flex h-auto w-[280px] xs:w-[360px] md:w-[420px] xl:w-[560px]'/>
+      <Custom className='flex h-auto w-[280px] xs:w-[360px] md:w-[420px] xl:w-[560px]'/>
     </div>
   )
 }
