a192ab1799638d2b3abc6f330cc35ef5af952d72
brockgingell <brockg68@icloud.com>
2023-10-22 12:54:34 -0400

    hrsrh

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { siteConfig } from '@/config/site'
 import { buttonVariants } from '@/components/ui/button'   
 import { motion } from 'framer-motion'
-import Heropic from 'public/HeroFinal.svg'
+import Heropic from '/public/HeroFinal.svg'
 
 export default function Hero() {
   return (
