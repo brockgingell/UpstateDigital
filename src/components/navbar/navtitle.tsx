fbbdb68b6a427a2a52a470139e27ba8e75bbe9d9
brockgingell <brockg68@icloud.com>
2023-10-30 05:26:26 -0400

    try

--- src/components/navbar/navtitle.tsx
+++ src/components/navbar/navtitle.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react' 
 import Link from 'next/link'
 import Image from 'next/image'
-import Icon from '/public/icon-new.svg'
+import Icon from '/public/IconFinal.svg'
 import { fontMono } from '@/lib/fonts'
 
 const NavTitle = () => {
